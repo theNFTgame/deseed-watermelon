49f76a8a32f4d889acdc88b2473ffb42e72a4e80
vitrum <xingang.zhu@sy-fintech.com>
2018-07-05 16:26:20 +0800

    update bg

--- phaser/src/scenes/simple-scene.js
+++ phaser/src/scenes/simple-scene.js
@@ -7,7 +7,9 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.image(160, 100, 'bg');
+    let bg = this.add.image(160, 100, 'bg');
+    bg.width = game.world.width;
+    bg.height = game.world.height;
     this.add.text(100, 100, 'Deseed!', { fill: '#0f0' });
     this.add.image(100, 200, 'cokecan');
     this.add.image(200, 200, 'seed');
